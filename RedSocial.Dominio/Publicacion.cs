871a65c04b9b246e62920ae77125e40121f326dd
Eric Vladimir Ramirez Betancur <ericr2617@hotmail.com>
2018-10-21 17:01:08 -0500

    Publicacion

---
+++ RedSocial.Dominio/Publicacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocial.Dominio
+{
+    [Table("Publicacion")]
+    public class Publicacion
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Usuario { get; set; }
+
+        [MaxLength(200)]
+        [Required]
+        public string Descripcion { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public int MeGusta { get; set; }
+
+        public int MeDigusta { get; set; }
+
+        public int VecesCompartido { get; set; }
+
+        public bool EsPrivada { get; set; }
+
+    }
+}
